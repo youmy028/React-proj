a4f13284c364a419055b156551d73db7c10315ed
youmy028 <youmy028@naver.com>
2022-12-13 20:28:25 +0900

    일시정지

--- src/stopWatch.js
+++ src/stopWatch.js
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 function StopWatch(){
     const [num, setNum] = useState(0);
     
-    setTimeout(() => setNum(num + 1),1000);
+    const timeOutId = setTimeout(() => setNum(num + 1),1000);
 
+    const pause = () =>{
+        clearTimeout(timeOutId);
+    };
 
-    return <>숫자: {num}</>;
+    return(
+     <>
+        숫자: {num}
+        <hr/>
+        <button onClick={pause}>일시정지</button>
+     </>
+    );
 }
 
 export default StopWatch;
