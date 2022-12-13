fe6b8fc8dcc490fb084345dbf1ce89d1b1ac10b1
youmy028 <youmy028@naver.com>
2022-12-13 19:44:54 +0900

    setTimeOut

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import stopWatch from './stopWatch';
+import StopWatch from './stopWatch';
 function App() {
   return (
     <>
-      <stopWatch/>
+      <StopWatch/>
     </>
   );
 }
