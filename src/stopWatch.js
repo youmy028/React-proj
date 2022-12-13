6f93064c2ad0fc0f97f30aed486b6aa35c1c3970
youmy028 <youmy028@naver.com>
2022-12-13 20:45:35 +0900

    재개 후 즉시 일시정지 버튼을 누르면 안되는 버그 수정

--- src/stopWatch.js
+++ src/stopWatch.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 function StopWatch(){
     const [num, setNum] = useState(0);
     
-    const timeOutId = setTimeout(() => setNum(num + 1),1000);
+    let timeOutId = setTimeout(() => setNum(num + 1),1000);
 
     const pause = () =>{
         clearTimeout(timeOutId);
     };
     const resume =() =>{
-        setTimeout(() => setNum(num + 1),1000);
+        timeOutId = setTimeout(() => setNum(num + 1),1000);
     };
 
     return(
