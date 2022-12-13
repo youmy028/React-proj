3f9a3195e7d1d9c39b98f0512fdd7dae550d76d6
youmy028 <youmy028@naver.com>
2022-12-13 20:37:24 +0900

    재개

--- src/stopWatch.js
+++ src/stopWatch.js
@@ -7,12 +7,17 @@ function StopWatch(){
     const pause = () =>{
         clearTimeout(timeOutId);
     };
+    const resume =() =>{
+        setTimeout(() => setNum(num + 1),1000);
+    };
 
     return(
      <>
         숫자: {num}
         <hr/>
         <button onClick={pause}>일시정지</button>
+        <hr/>
+        <button onClick={resume}>재개</button>
      </>
     );
 }
